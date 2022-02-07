c98c73823495f7fb584a1884db0da383e0aa2b00
dinosaur96m <nccadmantinaos@gmail.com>
2022-02-07 12:54:27 -0500

    important edit in NewItemJS

--- screens/items/NewItem.js
+++ screens/items/NewItem.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 
-export default function Item () {
+export default function NewItem () {
 
     return(
         <Text>Upload a new Item here</Text>
