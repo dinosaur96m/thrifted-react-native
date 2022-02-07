83400f869fabc5a0800e56006eb5cd746da93d06
dinosaur96m <nccadmantinaos@gmail.com>
2022-02-07 13:02:47 -0500

    consistent style & bp in all views

--- screens/items/Browse.js
+++ screens/items/Browse.js
@@ -1,6 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { textShadowColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import { textShadowColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes'
 
 export default function Browse () {
 
---
+++ screens/profile/Store.js
@@ -0,0 +1,10 @@
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+
+
+export default function Store () {
+
+    return(
+        <Text>Your Store Here</Text>
+    )
+}
