eecb0d003802bbecad726ce98775e94272bb0d76
dinosaur96m <nccadmantinaos@gmail.com>
2022-02-17 11:33:50 -0500

    styled profile screen

--- screens/profile/Profile.js
+++ screens/profile/Profile.js
@@ -1,23 +1,59 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View, Button } from 'react-native'
 import { textShadowColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
+import { FontAwesome5, Feather, MaterialCommunityIcons, Ionicons } from '@expo/vector-icons'
+import { bounce } from 'react-native/Libraries/Animated/Easing';
 
 export default function Profile ({navigation}) {
 
     return(
-        <View>
-            <Text>This is the Profile Page</Text>
-            <Button 
-                title="My Store"
-                onPress={ () => navigation.navigate('Store')}/>
-            <Button 
-                title="My Cart"
-                onPress={() => navigation.navigate('Cart')}
-            />
-            <Button 
-                title="Sell"
-                onPress={() => navigation.navigate('New Item')}
-            />
+        <View style={styles.profileScreen}>
+            <View style={styles.header}>
+                <Ionicons name="person-circle-outline" size={150} color="#f2cbbf"/>
+                <Text style={styles.username}>User's Profile</Text>
+            </View>
+            <View style={styles.buttonContainer}>
+                <FontAwesome5.Button
+                    name="store"
+                    style={styles.navButton}
+                    onPress={ () => navigation.navigate('Store')}>Your Store</FontAwesome5.Button>
+                <Feather.Button
+                    name="shopping-cart"
+                    style={styles.navButton}
+                    onPress={() => navigation.navigate('Cart')}
+                >Your Cart</Feather.Button>
+                <MaterialCommunityIcons.Button
+                    name="tag-plus"
+                    title="Sell"
+                    style={styles.navButton}
+                    onPress={() => navigation.navigate('New Item')}
+                >Sell</MaterialCommunityIcons.Button>
+            </View>
         </View>
     )
 }
+
+let styles = StyleSheet.create({
+    profileScreen: {
+        flex: 1
+    },
+    header: {
+        flex: 2,
+        alignItems: 'center',
+    },
+    username: {
+        fontWeight: "bold",
+        fontSize: 20,
+        marginTop: 15
+    },
+    buttonContainer: {
+        flex: 3,
+        justifyContent: 'space-around',
+        alignItems: 'center'
+    },
+    navButton: {
+        height: 60,
+        backgroundColor: '#f28361',
+        textAlign: 'center'
+    }
+})
