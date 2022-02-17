20477acb320f981aff72a6f37a6f55809fb7d96b
dinosaur96m <nccadmantinaos@gmail.com>
2022-02-17 10:34:05 -0500

    styled nav bar

--- screens/Home.js
+++ screens/Home.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View, Button } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { textShadowColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes'
+import { Ionicons } from '@expo/vector-icons'
 
 // import views
 import Browse from './items/Browse'
@@ -10,13 +11,31 @@ import Profile from './profile/Profile'
 
 const Tab = createBottomTabNavigator()
 
-export default function Home ({navigation}) {
+export default function Home({ navigation }) {
 
-    return(
-            <Tab.Navigator>
-                <Tab.Screen name="Browse" component={Browse}/>
-                <Tab.Screen name="Profile" component={Profile}/>
-            </Tab.Navigator>
+    return (
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
+
+                    if (route.name === 'Browse') {
+                        iconName = focused
+                            ? 'md-shirt'
+                            : 'md-shirt-outline';
+                    } else if (route.name === 'Profile') {
+                        iconName = focused ? 'md-person-circle' : 'md-person-circle-outline';
+                    }
+                    // You can return any component that you like here!
+                    return <Ionicons name={iconName} size={size} color={color} />;
+                },
+                tabBarActiveTintColor: 'tomato',
+                tabBarInactiveTintColor: 'gray',
+            })}
+        >
+            <Tab.Screen name="Browse" component={Browse} />
+            <Tab.Screen name="Profile" component={Profile} />
+        </Tab.Navigator>
 
     )
 }
